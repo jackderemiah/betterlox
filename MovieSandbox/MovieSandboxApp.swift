66e8dbcea87bae2512a95a7c7c34ca6aea7a4ea3
AWAY2021 <jderemiah@mail.sfsu.edu>
2023-03-16 10:35:42 -0700

    Initial Commit

---
+++ MovieSandbox/MovieSandboxApp.swift
@@ -0,0 +1,17 @@
+//
+//  MovieSandboxApp.swift
+//  MovieSandbox
+//
+//  Created by Jack  on 3/16/23.
+//
+
+import SwiftUI
+
+@main
+struct MovieSandboxApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
